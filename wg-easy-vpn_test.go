71c63085e28009178f4adc7a828449459a7489d7
asr <alban.siffer@irisa.fr>
2020-04-14 14:44:02 +0200

    add function to generate doc

--- wg-easy-vpn_test.go
+++ wg-easy-vpn_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"os"
 	"strings"
+	"testing"
 )
 
 const (
@@ -10,6 +13,55 @@ const (
 	titlePad  = "-"
 	// docFile   = "wg-easy-vpn.ex"
 )
+
+var manPageTemplate = `
+.ll 80
+.TH wg-easy-vpn 8 "April 14 2020" wg-easy-vpn "Wireguard Easy VPN"
+
+.SH NAME
+{{.Name}} \- {{.Usage}}
+.SH SYNOPSIS
+.B {{.Name}}
+[ \fICOMMAND\fP ] [ \fIOPTIONS\fP ] {{raw "<"}}\fI{{.ArgsUsage}}\fP{{raw ">"}}
+.BR
+
+.SH DESCRIPTION
+.ll 80
+{{.Description}}
+
+.SH COMMANDS
+
+.nf
+.ta 15 {{range .Commands}}
+\fB{{.Name}}\fP 	{{.Usage}} {{end}}
+
+.SH OPTIONS
+{{range .Commands}}
+\fB{{.Name}}\fP
+.nf
+.ta 30
+.in 10 {{range .Flags}} 
+--{{.Name}}{{if .Aliases}}{{range .Aliases}},-{{.}}{{end}}{{end}} {{if .DefaultText}}{{raw "<"}}\fI{{.DefaultText}}\fP{{raw ">"}}{{end}}	{{.Usage}}{{end}}
+.in
+{{end}}
+
+.SH SEE ALSO
+.BR wg (1).
+.BR wg-quick (1),
+
+.SH AUTHOR
+.B {{.Name}}
+was written by {{range .Authors}}
+.MT {{.Email}}
+{{.Name}} 
+.ME
+{{end}}
+
+.SH COPYRIGHT
+{{.Copyright}}
+.
+
+`
 
 func title(s string) {
 	length := len(s) + 2
@@ -26,15 +78,30 @@ func title(s string) {
 		strings.Repeat(titlePad, rightLength))
 }
 
-// func TestDoc(t *testing.T) {
-// 	f, err := os.Create(docFile)
-// 	if err != nil {
-// 		t.Errorf("Error while opening %s (%v)", docFile, err)
-// 	}
-// 	if md, err := app.ToMan(); err == nil {
-// 		f.WriteString(md)
-// 	}
-
-// 	// fmt.Println(app.ToMan())
-// 	f.Close()
-// }
+func TestGenDoc(t *testing.T) {
+	manFile := "/tmp/wg-easy-vpn.ex"
+	f, err := os.Create(manFile)
+	if err != nil {
+		t.Error(err)
+	}
+	// if md, err := app.ToMan(); err == nil {
+	// 	f.WriteString(md)
+	// } else {
+	// 	t.Error(err)
+	// }
+	tp := template.New("man")
+	tp.Funcs(template.FuncMap{
+		"raw": func(html string) template.HTML {
+			return template.HTML(html)
+		},
+	})
+	tp, err = tp.Parse(manPageTemplate)
+	if err != nil {
+		t.Error(err)
+	}
+	// remove \n and \t
+	app.Description = strings.Replace(app.Description, "\n", "", -1)
+	app.Description = strings.Replace(app.Description, "\t", "", -1)
+	tp.ExecuteTemplate(f, "man", app)
+	f.Close()
+}
